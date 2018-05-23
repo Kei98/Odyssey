206d4f23686296b94c84e8c1006ad5643af11dbe
Jonathana1196 <jonathana1196@gmail.com>
2018-05-23 17:30:30 -0600

    Ordenamientos comentados y actualizados.

--- com.AyJK.Odyssey/src/com/AyJK/sort/BubbleSort.java
+++ com.AyJK.Odyssey/src/com/AyJK/sort/BubbleSort.java
@@ -1,10 +1,65 @@
 package com.AyJK.sort;
 
-import com.AyJK.list.DoubleLL;
+import com.AyJK.list.Node;
+import com.AyJK.list.SimpleLL;
 
+/**
+ * Clase en la que se crea el método de ordenamiento BubbleSort.
+ * 
+ * @author LKJ
+ *
+ * @param <T>:
+ *            Parámetro genérico del BubbleSort.
+ */
 public class BubbleSort<T extends Comparable<T>> {
 
-    public void bubbleSort(DoubleLL<T> list) {
+    /**
+     * Variables que serán utilizadas en la clase.
+     */
+    private T temp;
 
+    /**
+     * Constructor de la clase.
+     */
+    public BubbleSort() {
+    }
+
+    /**
+     * Método que recibe como parámetro una lista simple que ordenará utilizando la
+     * lógica del BubbleSort.
+     * 
+     * @param list:
+     *            lista que se desear ordenar mediante BubbleSort.
+     */
+    public void bubbleSort(SimpleLL<T> list) {
+	for (int i = 0; i < list.getSize() - 1; i++) {
+	    Node<T> aux = list.getHead();
+	    Node<T> next = list.getHead().getNext();
+	    for (int j = 1; j < list.getSize() - 1; j++) {
+		if (aux.getData().compareTo(next.getData()) > 0) {
+		    temp = aux.getData();
+		    aux.setData(next.getData());
+		    next.setData(temp);
+		}
+		aux = next;
+		next = next.getNext();
+	    }
+	}
+    }
+
+    /**
+     * Método que se encarga de mostrar en consola el arreglo.
+     * 
+     * @param list:
+     *            arreglo que se desea imprimir.
+     */
+    public void printL(SimpleLL<T> list) {
+	System.out.print("[");
+	for (int i = 0; i < list.getSize() - 1; i++) {
+	    System.out.print(list.getDatai(i) + ", ");
+	}
+	System.out.print(list.getDatai(list.getSize() - 1));
+	System.out.print("]");
+	System.out.println(" ");
     }
 }
--- com.AyJK.Odyssey/src/com/AyJK/sort/QuickSort.java
+++ com.AyJK.Odyssey/src/com/AyJK/sort/QuickSort.java
@@ -1,5 +1,93 @@
 package com.AyJK.sort;
 
+import com.AyJK.list.SimpleLL;
+
+/**
+ * Clase en la que se crea el método de ordenamiento QuickSort.
+ * 
+ * @author LKJ
+ *
+ * @param <T>:
+ *            Parámetro genérico del QuickSort.
+ */
 public class QuickSort<T extends Comparable<T>> {
 
+    /**
+     * Variables que serán utilizadas en la clase.
+     */
+    private int i;
+    private int j;
+    private T pivot;
+    private T temp;
+
+    /**
+     * Constructor de la clase.
+     */
+    public QuickSort() {
+    }
+
+    /**
+     * Método que se encarga de ordenar una lista utilizando la lógica del metodo de
+     * ordenamiento QuickSort.
+     * 
+     * @param array:
+     *            arreglo al cual se le desea ordenar mediante el método QuickSort.
+     */
+    public void quickSort(SimpleLL<T> array) {
+	quickSort(array, 0, array.getSize() - 1);
+    }
+
+    /**
+     * Método que se encarga de ordenar un arreglo de la forma QuickSort.
+     * 
+     * @param array:
+     *            arreglo que se desea ordenar.
+     * @param first:
+     *            primera pocisión en el arreglo.
+     * @param last:
+     *            última posición en el arreglo.
+     */
+    private void quickSort(SimpleLL<T> array, int first, int last) {
+	i = first;
+	j = last;
+	pivot = array.getDatai((int) ((i + j) / 2));
+
+	do {
+	    while (array.getDatai(i).compareTo(pivot) < 0) {
+		i++;
+	    }
+	    while (array.getDatai(j).compareTo(pivot) > 0) {
+		j--;
+	    }
+	    if (i <= j) {
+		temp = array.getDatai(i);
+		array.setDatai(i, array.getDatai(j));
+		array.setDatai(j, temp);
+		i++;
+		j--;
+	    }
+	} while (i <= j);
+	if (first < j) {
+	    quickSort(array, first, j);
+	}
+	if (i < last) {
+	    quickSort(array, i, last);
+	}
+    }
+
+    /**
+     * Método que se encarga de mostrar en consola el arreglo.
+     * 
+     * @param array:
+     *            arreglo que se desea imprimir en consola.
+     */
+    public void printL(SimpleLL<T> array) {
+	System.out.print("[");
+	for (int i = 0; i < array.getSize() - 1; i++) {
+	    System.out.print(array.getDatai(i) + ", ");
+	}
+	System.out.print(array.getDatai(array.getSize() - 1));
+	System.out.print("]");
+	System.out.println(" ");
+    }
 }
