0aa44d4d88d538435bbdb418d1f31b044c980d6e
Jonathana1196 <jonathana1196@gmail.com>
2018-05-26 17:52:15 -0600

    -Actualización.-

--- com.AyJK.Odyssey/src/com/AyJK/sort/BubbleSort.java
+++ com.AyJK.Odyssey/src/com/AyJK/sort/BubbleSort.java
@@ -28,14 +28,14 @@ public class BubbleSort<T extends Comparable<T>> {
      * Método que recibe como parámetro una lista simple que ordenará utilizando la
      * lógica del BubbleSort.
      * 
-     * @param list:
+     * @param array:
      *            lista que se desear ordenar mediante BubbleSort.
      */
-    public void bubbleSort(SimpleLL<T> list) {
-	for (int i = 0; i < list.getSize() - 1; i++) {
-	    Node<T> aux = list.getHead();
-	    Node<T> next = list.getHead().getNext();
-	    for (int j = 1; j < list.getSize() - 1; j++) {
+    public void bubbleSort(SimpleLL<T> array) {
+	for (int i = 0; i < array.getSize() - 1; i++) {
+	    Node<T> aux = array.getHead();
+	    Node<T> next = array.getHead().getNext();
+	    for (int j = 1; j < array.getSize() - 1; j++) {
 		if (aux.getData().compareTo(next.getData()) > 0) {
 		    temp = aux.getData();
 		    aux.setData(next.getData());
@@ -50,15 +50,15 @@ public class BubbleSort<T extends Comparable<T>> {
     /**
      * Método que se encarga de mostrar en consola el arreglo.
      * 
-     * @param list:
+     * @param array:
      *            arreglo que se desea imprimir.
      */
-    public void printL(SimpleLL<T> list) {
+    public void printL(SimpleLL<T> array) {
 	System.out.print("[");
-	for (int i = 0; i < list.getSize() - 1; i++) {
-	    System.out.print(list.getDatai(i) + ", ");
+	for (int i = 0; i < array.getSize() - 1; i++) {
+	    System.out.print(array.getDatai(i) + ", ");
 	}
-	System.out.print(list.getDatai(list.getSize() - 1));
+	System.out.print(array.getDatai(array.getSize() - 1));
 	System.out.print("]");
 	System.out.println(" ");
     }
